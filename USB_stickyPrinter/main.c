d190d285deee0205256456dadcbe20c6c1d0bdda
Scott Christensen <mscottchristensen@gmail.com>
2017-01-28 19:12:08 -0500

    documented the watchdog timer and internal clock control. made a working example of how to use the watchdog interrupt and quantified the time error of the internal oscillator to about 2.5%

--- USB_stickyPrinter/main.c
+++ USB_stickyPrinter/main.c
@@ -6,8 +6,24 @@
 
 #include <msp430.h>				
 
+volatile int milleseconds;
+
 int main(void) {
-	WDTCTL = WDTPW | WDTHOLD;		// Stop watchdog timer
+	// Set watchdog to timer mode with 8ms/1_MHz_DCO period interrupts
+	WDTCTL = WDT_MDLY_8;
+	// enable the watchdog interrupt timer
+	IE1 = WDTIE;
+
+	//set the half millisecond counter to zero
+	milleseconds = 0;
+
+	//Change DCO frequency to 16 MHz
+	BCSCTL1 = CALBC1_16MHZ;
+	DCOCTL  = CALDCO_16MHZ;
+
+	//set both masterclock AND sub master clock sources to DCO
+	//make the masterclock oscilate at a period of DCO/1 and the SMCLK at DCO/2
+	BCSCTL2 = DIVS_1;
 
 	// I/0 functionality on all pins
 	P1SEL  = 0x00;					// Set P1.0-P1.7 to I/O functionality
@@ -19,30 +35,40 @@ int main(void) {
 	P1DIR = 0xff;					// Set P1.0-P1.7 to output direction
 	P2DIR = 0xff;					// Set P2.0-P2.7 to output direction
 
-	P1OUT = BIT6; 					// Turn on only the green LED on P1.6
+	P1OUT = 0x00;
+
+	//enable interrupts (they won't run unless you do this)
+	_EINT();
 
 	for(;;) {
-		volatile unsigned int i;	// volatile to prevent optimization
-		volatile unsigned int j;	// volatile to prevent optimization
-
-		P1OUT ^= BIT0;				// Toggle P1.0 using exclusive-OR
-
-
-		i = 5;						// SW Delay
-
-		do
-		{
-			P1OUT ^= BIT6;			// Toggle P1.6 using exclusive-OR
-			i--;
-			j = 20000;				// Scale the delay
-			do
-			{
-				j--;
-			}
-			while (j != 0);
-		}
-		while(i != 0);
+		//infinite loop
 	}
 	
 	return 0;
+}
+
+#pragma vector=WDT_VECTOR
+__interrupt void WatchDog_Timer(void)
+{
+	// real time is about 1.0252777777777777777777777777778 times faster than my time
+	// the data sheet documents the lack of precision on the internal oscillator so no surprises here, 2.5% is pretty good
+
+	//another half millisecond has passed
+	milleseconds++;
+
+	//just blink
+	if(milleseconds == 950)
+		P1OUT ^= BIT6;
+	if(milleseconds == 1000)
+		P1OUT ^= BIT6;
+
+	//a full second has passed
+	if(milleseconds == 1000)
+	{
+		P1OUT ^= BIT0;
+		milleseconds = 0;
+	}
+
+	//clear the interrupt flag on the way out
+	IFG1 &= ~WDTIFG;
 }
